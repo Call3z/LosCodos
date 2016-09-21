7a22afc4de795db224b6996cf074f831d78fcd57
Call3z <electrodez@gmail.com>
2016-09-21 11:19:20 +0200

    Product Done

--- Webshop Site/Interfaces/IProduct.cs
+++ Webshop Site/Interfaces/IProduct.cs
@@ -8,5 +8,9 @@ namespace Webshop_Site.Interfaces
 {
     interface IProduct
     {
+        string Brand { get; set; }
+        string Color { get; set; }
+        double Price { get; set; }
+        string Size { get; set; }
     }
 }
