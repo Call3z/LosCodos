4923e4f64b2d2cee5b1e819c67aa629427faac88
Call3z <electrodez@gmail.com>
2016-09-21 13:40:09 +0200

    Tankar

--- Webshop Site/Interfaces/IClothingDataBase.cs
+++ Webshop Site/Interfaces/IClothingDataBase.cs
@@ -11,6 +11,11 @@ namespace Webshop_Site.Interfaces
 {
     public interface IClothingDataBase
     {
-        
+        //todo Path till "clothes.xml" eller vad nu databasen kan komma att heta?
+        //todo Lista av IProduct??
+
+        //todo om kläderna inte skall hårdkodas, kanske vi ska ha en GetClothesFromDatabase? där vi hämtar kläderna ur databasen och lägger in dem i Listan av IProduct?
+
+        //todo En funktion som sparar ett objekt av IProduct till databasen? detta blir ju väldigt väsentligt om vi skall använda oss av admingränssnitt.
     }
 }
