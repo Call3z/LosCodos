e45afbc6dfe0b114fce529d5b5059d83850c7040
Call3z <electrodez@gmail.com>
2016-09-21 13:13:39 +0200

    C

--- Webshop Site/Classes/Checkout.cs
+++ Webshop Site/Classes/Checkout.cs
@@ -2,10 +2,11 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using Webshop_Site.Interfaces;
 
 namespace Webshop_Site.Classes
 {
-    public class Checkout :
+    public class CheckOut : ICheckOut
     {
 
     }
