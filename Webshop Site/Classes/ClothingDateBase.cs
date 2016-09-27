bd1e057793293886050ff3bac54ef4174de07cb7
andreholmlund <andre.holmlund@gmail.com>
2016-09-27 14:51:17 +0200

    Interface

--- Webshop Site/Classes/ClothingDateBase.cs
+++ Webshop Site/Classes/ClothingDateBase.cs
@@ -13,33 +13,10 @@ namespace Webshop_Site.Classes
     {
         public List<IProduct> wareHouse { get; set; }
 
-        public void SetDummyShoes(Product list)
-        {
-            list.Brand = "Nike";
-            list.Color = "Blå";
-            list.Price = 299;
-            list.Size = "42";
-        }
-
-        public void SetDummyShirts(Product list)
-        {
-            list.Brand = "Tiger of sweden";
-            list.Color = "Svart";
-            list.Price = 1500;
-            list.Size = "M";
-        }
-
-        public void SetDummyPants(Product list)
-        {
-            list.Brand = "Crocker";
-            list.Color = "Blå";
-            list.Price = 250;
-            list.Size = "30/32";
-        }
 
         XmlDocument reader = new XmlDocument();
 
-        public List<IProduct>ProductShoesList()
+        public List<IProduct>GetProducts()
         {
             List<IProduct> temp = new List<IProduct>();         
             reader.Load(Constants.PathToProductDatabaseXml());
